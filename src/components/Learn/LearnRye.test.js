39336d2d4e4ade57c8446fe17f2e38bb67c4a580
Julie <39500350+JGD625@users.noreply.github.com>
2019-10-13 16:59:31 -0400

    Tests Added

---
+++ src/components/Learn/LearnRye.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { LiquorListProvider } from '../../contexts/LiquorListContext'
+import { LiquorProvider } from '../../contexts/LiquorContext'
+import LearnRye from './LearnRye';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter>
+    <LiquorListProvider>
+      <LiquorProvider><LearnRye /></LiquorProvider>
+    </LiquorListProvider>
+  </BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
